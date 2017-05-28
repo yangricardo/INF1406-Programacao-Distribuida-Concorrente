905434e886c07fbe6a3e8c14c1e0d7f8c31c76a5
yangricardo <yang_ricardo@hotmail.com>
2017-05-28 18:11:16 -0300

    ServidorExecucao build 1
    - Atualização na implementação de Callback: implements Serializable

--- inf1406_trabalho3_1212206_servidor_execucao/src/contracts/Callback.java
+++ inf1406_trabalho3_1212206_servidor_execucao/src/contracts/Callback.java
@@ -1,8 +1,9 @@
 package contracts;
 
+import java.io.Serializable;
 import java.rmi.RemoteException;
 import java.util.concurrent.Callable;
 
-public interface Callback<T> extends Callable<T> {
+public interface Callback<T> extends Serializable,Callable<T> {
 	void entregaResultado(Resultado<T> resultado) throws RemoteException;
 }
