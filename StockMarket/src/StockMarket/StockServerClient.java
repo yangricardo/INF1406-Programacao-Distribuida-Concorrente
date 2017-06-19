b45d905e9fda418c1ab37fa7ab8d6b5d5b4a5c66
Eduardo Verdeja <eaverdeja@hotmail.com>
2017-06-19 19:34:25 -0300

    Implementando StockServer e StockClient

--- StockMarket/src/StockMarket/StockServerClient.java
+++ StockMarket/src/StockMarket/StockServerClient.java
@@ -7,8 +7,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
+import org.omg.CORBA.COMM_FAILURE;
 import org.omg.CORBA.ORB;
-
+import org.omg.CORBA.TRANSIENT;
 
 public class StockServerClient {
 
@@ -19,20 +20,31 @@ public class StockServerClient {
 		orbProps.setProperty("org.omg.CORBA.ORBClass","org.jacorb.orb.ORB");
 		orbProps.setProperty("org.omg.CORBA.ORBSingletonClass", "org.jacorb.orb.ORBSingleton");
 		ORB orb = ORB.init(args, orbProps);
-		
+
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
 			String ior = reader.readLine();
-			
+
 			org.omg.CORBA.Object obj = orb.string_to_object(ior);
-			
-			
+			StockServer server = StockServerHelper.narrow(obj);
+
+			String[] stockSymbols = server.getStockSymbols();
+			for(int i = 0; i < stockSymbols.length; i++) {
+				System.out.println(stockSymbols[i] + " : " + server.getStockValue(stockSymbols[i]));
+			}
+
 		} catch (FileNotFoundException e) {
 			System.err.println("Arquivo inexistente:\n"+e);
 		} catch (IOException e) {
 			System.err.println("Erro ao ler arquivo:\n"+e);
+		} catch (UnknownSymbol unknownSymbol) {
+			unknownSymbol.printStackTrace();
+		} catch (TRANSIENT e) {
+			System.err.println("O serviço encontra-se indisponível");
+		} catch (COMM_FAILURE e) {
+			System.err.println("Falha de comunicação com o serviço");
 		}
-		
+
 	}
 
 }
